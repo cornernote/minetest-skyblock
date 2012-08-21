d9a39c0c57521137049ee4b60e3d89f4dc32ceb4
Brett O'Donnell <cornernote@gmail.com>
2012-08-21 18:30:12 +0930

    jungletree can be made into wood

--- register_craft.lua
+++ register_craft.lua
@@ -135,3 +135,12 @@ minetest.register_craft({
 		{"default:desert_stone"},
 	}
 })
+
+-- jungletree turns to wood
+minetest.register_craft({
+	output = "default:wood 4",
+	recipe = {
+		{"default:jungletree"},
+	}
+})
+
