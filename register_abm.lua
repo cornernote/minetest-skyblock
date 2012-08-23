b8b1428891396330b6173ad56e1a3b067a2ffc49
Brett O'Donnell <cornernote@gmail.com>
2012-08-23 16:19:21 +0930

    tweak spawn and grow for plants

--- register_abm.lua
+++ register_abm.lua
@@ -15,26 +15,65 @@ REGISTER ABM
 minetest.register_abm({
 	nodenames = "default:sapling",
 	interval = 30,
-	chance = 5,
+	chance = 10,
 	action = function(pos)
 		skyblock.generate_tree(pos)
     end
 })
 
--- junglegrass/dry_shrub spawns on sand, desert_sand and dirt_with_grass
+-- junglegrass and dry_shrub spawns on sand, desert_sand and dirt_with_grass
 minetest.register_abm({
 	nodenames = {"default:sand", "default:desert_sand", "default:dirt_with_grass"},
-	interval = 150,
-	chance = 10,
+	interval = 300,
+	chance = 100,
 	action = function(pos, node)
 		pos.y = pos.y+1
 		if minetest.env:get_node(pos).name == "air" and minetest.env:find_node_near(pos, 4, {"default:dry_shrub","default:junglegrass"})==nil then
-			if math.random(1,10) > 4 then
+			if math.random(1,5) > 2 then
 				minetest.env:set_node(pos, {name="default:dry_shrub"})
 			else
 				minetest.env:set_node(pos, {name="default:junglegrass"})
 			end
 		end
+	end
+})
+
+-- papyrus spawns on dirt_with_grass next to water_source
+minetest.register_abm({
+	nodenames = {"default:dirt_with_grass"},
+	neighbors = {"default:water_source"},
+	interval = 300,
+	chance = 100,
+	action = function(pos, node)
+		-- check for space
+		for i=1,2 do
+			if minetest.env:get_node({x = pos.x, y = pos.y + i, z = pos.z}).name ~= "air" then
+				return
+			end
+		end
+		-- spawn
+		minetest.env:set_node({x = pos.x, y = pos.y + 1, z = pos.z}, {name="default:papyrus"})
+	end
+})
+
+-- papyrus grows upto 3 in height
+minetest.register_abm({
+	nodenames = {"default:papyrus"},
+	interval = 100,
+	chance = 50,
+	action = function(pos, node)
+		-- check for space
+		for i=1,2 do
+			if minetest.env:get_node({x = pos.x, y = pos.y + i, z = pos.z}).name ~= "air" then
+				return
+			end
+		end
+		-- check height
+		if minetest.env:get_node({x = pos.x, y = pos.y - 3, z = pos.z}).name == "default:papyrus" then
+			return
+		end
+		-- grow
+		minetest.env:set_node({x = pos.x, y = pos.y + 1, z = pos.z}, {name="default:papyrus"})
 	end
 })
 
@@ -44,28 +83,39 @@ minetest.register_abm({
 	interval = 300,
 	chance = 150,
 	action = function(pos, node)
-		for y=0,math.random(1,4) do
-			pos.y = pos.y+1
-			if minetest.env:get_node(pos).name == "air" and minetest.env:find_node_near(pos, 6, {"default:cactus"})==nil then
-				minetest.env:set_node(pos, {name="default:cactus"})
+		-- check for space
+		for i=1,2 do
+			if minetest.env:get_node({x = pos.x, y = pos.y + i, z = pos.z}).name ~= "air" then
+				return
 			end
 		end
+		-- check for nearby
+		if minetest.env:find_node_near(pos, 6, {"default:cactus"}) ~= nil then
+			return
+		end
+		-- spawn
+		minetest.env:set_node({x = pos.x, y = pos.y + 1, z = pos.z}, {name="default:cactus"})
 	end
 })
 
--- papyrus spawns on dirt and dirt_with_grass if next to water
+-- cactus grows upto 4 in height
 minetest.register_abm({
-	nodenames = {"default:dirt", "default:dirt_with_grass"},
-	neighbors = {"default:water_source", "default:water_flowing"},
-	interval = 300,
+	nodenames = {"default:cactus"},
+	interval = 200,
 	chance = 50,
 	action = function(pos, node)
-		for y=0,math.random(1,4) do
-			pos.y = pos.y+1
-			if minetest.env:get_node(pos).name == "air" then
-				minetest.env:set_node(pos, {name="default:papyrus"})
+		-- check for space
+		for i=1,2 do
+			if minetest.env:get_node({x = pos.x, y = pos.y + i, z = pos.z}).name ~= "air" then
+				return
 			end
 		end
+		-- check height
+		if minetest.env:get_node({x = pos.x, y = pos.y - 4, z = pos.z}).name == "default:cactus" then
+			return
+		end
+		-- grow
+		minetest.env:set_node({x = pos.x, y = pos.y + 1, z = pos.z}, {name="default:cactus"})
 	end
 })
 
