ffd25cb02e189c446341ed18a101538f9d6c7eb9
Sokomine <wegwerf@anarres.dyndns.org>
2015-06-08 07:37:42 +0200

    junglewood and pinewood are now diffrent wood types for more variety

--- skyblock/register_craft.lua
+++ skyblock/register_craft.lua
@@ -137,12 +137,12 @@ minetest.register_craft({
 })
 
 -- jungletree turns to wood
-minetest.register_craft({
-	output = "default:wood 4",
-	recipe = {
-		{"default:jungletree"},
-	}
-})
+--minetest.register_craft({
+--	output = "default:wood 4",
+--	recipe = {
+--		{"default:jungletree"},
+--	}
+--})
 
 -- mese
 minetest.register_craft({
