74fdc88d074796b3dc7e733c37e1be1a124f0630
Sokomine <wegwerf@anarres.dyndns.org>
2015-06-10 02:19:31 +0200

    track eating actions

--- skyblock/register_misc.lua
+++ skyblock/register_misc.lua
@@ -42,6 +42,11 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode)
 	achievements.on_placenode(pos, newnode, placer, oldnode)
 end)
 
+-- track eating actions
+minetest.register_on_item_eat(function(hp_change, replace_with_item, itemstack, user, pointed_thing)
+	achievements.on_item_eat( hp_change, replace_with_item, itemstack, user, pointed_thing )
+end)
+
 -- register the game after the server starts
 minetest.after(5, function()
 
